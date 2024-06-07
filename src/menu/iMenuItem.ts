1a5f7ff3520ae7cb15de893bc19ec09383c6d8af
jhowrf <jhowrf@gmail.com>
2024-06-07 17:20:44 -0300

    refac: abstract menu items

---
+++ src/menu/iMenuItem.ts
@@ -0,0 +1,5 @@
+export interface iMenuItem {
+    routeName: string,
+    icon: string,
+    label: string
+}
---
+++ src/menu/sanAndreas/saMainMenu.ts
@@ -0,0 +1,114 @@
+import {iMenuItem} from "@/menu/iMenuItem"
+
+export const saMainMenuItems: Array<iMenuItem> = [
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Introdução'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Coisas Necessárias para o 100%'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Missões Principais'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Serviços Secundários'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Missões de Veículos'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Missões de Entregador'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Todas as Propriedades'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Ostras'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Pichações'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Fotos'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Ferradura'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Escolas'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Importação/Exportação de Veículos'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Corridas'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Desafios'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Corridas de Estádio'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Academias'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Estande de tiro'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Recompensas 100%'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Créditos'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Troféus e Conquistas'
+    },
+    {
+        routeName: 'in-development',
+        icon: 'map',
+        label: 'Armas'
+    },
+]
