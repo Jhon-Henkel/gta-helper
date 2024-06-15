dd3ca617d420377231af99cb476a69f4e10ea429
Jhon-Henkel <Jhon-Henkel@users.noreply.github.com>
2024-06-15 18:04:24 +0000

    deploy: 93b817fc5f3197e42529a64b16029673e348368e

--- assets/status-tap-legacy-BxT3zZD5.js
+++
@@ -1,5 +0,0 @@
-System.register(["./index-legacy-BQ5RBHHL.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.T,r=e.U,s=e.V,o=e.W,i=e.X}],execute:function(){
-/*!
-             * (C) Ionic http://ionicframework.com - MIT License
-             */
-e("startStatusTap",(()=>{const e=window;e.addEventListener("statusTap",(()=>{n((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const c=r(a);c&&new Promise((e=>s(c,e))).then((()=>{o((async()=>{c.style.setProperty("--overflow","hidden"),await i(c,300),c.style.removeProperty("--overflow")}))}))}))}))}))}}}));
