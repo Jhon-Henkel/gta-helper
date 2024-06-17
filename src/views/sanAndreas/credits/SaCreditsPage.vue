e97045310c57c9fd682ccd0f8e54a55d6db884f0
jhowrf <jhowrf@gmail.com>
2024-06-17 13:56:19 -0300

    feat: added credits page

--- src/views/sanAndreas/credits/SaCreditsPage.vue
+++ src/views/sanAndreas/credits/SaCreditsPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import GtaHelperPage from "@/components/page/GtaHelperPage.vue"
 import GtaHelperHeader from "@/components/header/GtaHelperHeader.vue"
-import { IonList, IonNote, IonLabel, IonItem } from "@ionic/vue"
+import {IonList, IonNote, IonLabel, IonItem} from "@ionic/vue"
 </script>
 
 <template>
@@ -16,19 +16,20 @@ import { IonList, IonNote, IonLabel, IonItem } from "@ionic/vue"
                     <ion-note slot="end">Jhonatan Henkel</ion-note>
                 </ion-item>
                 <ion-item>
-                    <ion-label>Links no qual peguei conteúdo:
-                    <p>
-                        <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=1974532552">steamcommunity</a>
-                    </p>
-                    <p>
-                        <a href="https://gta.fandom.com/pt/wiki/Miss%C3%B5es_do_GTA_San_Andreas">gta.fandom</a>
-                    </p>
-                    <p>
-                        <a href="https://www.gtaall.com.br/gta-san-andreas/missions-gta-san-andreas.html">gtaall</a>
-                    </p>
-                    <p>
-                        <a href="https://mapgenie.io/grand-theft-auto-san-andreas/checklist">mapgenie</a>
-                    </p>
+                    <ion-label>
+                        Links no qual peguei conteúdo:
+                        <p>
+                            <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=1974532552">steamcommunity</a>
+                        </p>
+                        <p>
+                            <a href="https://gta.fandom.com/pt/wiki/Miss%C3%B5es_do_GTA_San_Andreas">gta.fandom</a>
+                        </p>
+                        <p>
+                            <a href="https://www.gtaall.com.br/gta-san-andreas/missions-gta-san-andreas.html">gtaall</a>
+                        </p>
+                        <p>
+                            <a href="https://mapgenie.io/grand-theft-auto-san-andreas/checklist">mapgenie</a>
+                        </p>
                     </ion-label>
                 </ion-item>
             </ion-list>
