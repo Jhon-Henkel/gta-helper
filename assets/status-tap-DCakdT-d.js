ece5e5939997759147e0b773ef95322cac76faab
Jhon-Henkel <Jhon-Henkel@users.noreply.github.com>
2025-06-20 23:43:06 +0000

    deploy: b2f7d29c931f3d7ffcf81e67033e75c8ded3c099

--- assets/status-tap-DCakdT-d.js
+++
@@ -1,3 +0,0 @@
-import{c2 as a,c3 as i,c4 as c,c5 as d,c6 as l}from"./index-DJZmFhBu.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
---
+++ assets/status-tap-legacy-rtA2QS2H.js
@@ -0,0 +1,5 @@
+System.register(["./index-legacy-DXy-6WCr.js"],(function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.c2,r=e.c3,s=e.c4,o=e.c5,i=e.c6}],execute:function(){
+/*!
+             * (C) Ionic http://ionicframework.com - MIT License
+             */
+e("startStatusTap",(()=>{const e=window;e.addEventListener("statusTap",(()=>{n((()=>{const t=e.innerWidth,n=e.innerHeight,c=document.elementFromPoint(t/2,n/2);if(!c)return;const a=r(c);a&&new Promise((e=>s(a,e))).then((()=>{o((async()=>{a.style.setProperty("--overflow","hidden"),await i(a,300),a.style.removeProperty("--overflow")}))}))}))}))}))}}}));
